99512dfee0490246cc471421b7aee05441488966
Fabian Morales <135294869+FabianMorales12@users.noreply.github.com>
2024-03-09 12:16:35 -0600

    Add files via upload

---
+++ primer parcial/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("¡Bienvenido!");
+
+        string nombre, apellido, dominio, correoElectronico;
+
+        Console.Write("fabian: ");
+        nombre = Console.ReadLine();
+
+        Console.Write("morales: ");
+        apellido = Console.ReadLine();
+
+        Console.Write("@gmail.com: ");
+        dominio = Console.ReadLine();
+
+        correoElectronico = GenerarCorreoElectronico(nombre, apellido, dominio);
+
+        Console.WriteLine($"Su correo electrónico generado es: {correoElectronico}");
+    }
+
+    static string GenerarCorreoElectronico(string nombre, string apellido, string dominio)
+    {
+        string correo = $"{nombre.Substring(0, 3).ToLower()}{apellido.Substring(0, 3).ToLower()}@{dominio}";
+        return correo;
+    }
+}
